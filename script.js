09425493f8426ea3d7956cd2add1fbf4d9fa04d9
Rajashree Datar <125515685+r-datar@users.noreply.github.com>
2025-05-12 18:25:28 +0530

    Added changing bgcolor and opacity functionality

--- script.js
+++ script.js
@@ -39,6 +39,9 @@ function drawGrid(numSquares) {
             const div_name   = document.createElement("div");
             div_name.className = "square";
             div_name.style.cssText += `height: ${sqr_height}px; width: ${sqr_width}px`;
+
+            div_name.style.backgroundColor = "rgb(255,255,255)";
+            div_name.style.opacity = 0.1;
             div_name.id = "sqr_" + i + "_" + j;
             container_div.appendChild(div_name);
         }
@@ -46,12 +49,24 @@ function drawGrid(numSquares) {
     }
 }
 
+function randomRGB() {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    return `rgb(${r}, ${g}, ${b})`;
+  }
+
 container_div.addEventListener("mouseover", (event) => { 
     let sqr = event.target.id;
-    
     const hover_div = document.querySelector(`#${sqr}`)
-    
-    hover_div.style.cssText += "background-color : blue"; 
+   
+    let bgColor = randomRGB()
+    hover_div.style.cssText += `background-color : ${bgColor}`; 
+   
+    //increase the opacity by 0.1 for every interaction (max value 1.0)
+    let inc = Number(hover_div.style.opacity);
+    inc = inc >= 1.0 ? 0.1 : inc + 0.1;
+    hover_div.style.opacity = inc;
 })
 
 drawGrid(16)
